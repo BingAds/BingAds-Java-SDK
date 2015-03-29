67b83e6d3723ab561e163868729289693730d58c
eric-urban <eric.urban@microsoft.com>
2015-03-28 17:05:10 -0700

    add BulkPauseAdGroups sample

--- examples/BingAdsDesktopApp/src/main/java/com/microsoft/bingads/examples/CreateKeywordsAds.java
+++ examples/BingAdsDesktopApp/src/main/java/com/microsoft/bingads/examples/CreateKeywordsAds.java
@@ -1,17 +1,3 @@
-// Copyright 2015 Microsoft Corporation 
-
-// Licensed under the Apache License, Version 2.0 (the "License"); 
-// you may not use this file except in compliance with the License. 
-// You may obtain a copy of the License at 
-
-//    http://www.apache.org/licenses/LICENSE-2.0 
-
-// Unless required by applicable law or agreed to in writing, software 
-// distributed under the License is distributed on an "AS IS" BASIS, 
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-// See the License for the specific language governing permissions and 
-// limitations under the License. 
-
 package com.microsoft.bingads.examples;
 
 import java.rmi.*;
@@ -422,7 +408,7 @@ public class CreateKeywordsAds {
          for (BatchError error : partialErrors.getBatchErrors())
          {
              // The index of the partial errors is equal to the index of the list
-             // specified in the call to AddKeywords.
+             // specified in the call to AddAds.
 
              System.out.printf("\nAd[%d] (%s) not added due to the following error:\n", error.getIndex(), attributeValues[error.getIndex()]);
 
--- examples/BingAdsDesktopApp/src/main/java/com/microsoft/bingads/examples/Targets.java
+++ examples/BingAdsDesktopApp/src/main/java/com/microsoft/bingads/examples/Targets.java
@@ -1,17 +1,3 @@
-// Copyright 2015 Microsoft Corporation 
-
-// Licensed under the Apache License, Version 2.0 (the "License"); 
-// you may not use this file except in compliance with the License. 
-// You may obtain a copy of the License at 
-
-//    http://www.apache.org/licenses/LICENSE-2.0 
-
-// Unless required by applicable law or agreed to in writing, software 
-// distributed under the License is distributed on an "AS IS" BASIS, 
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-// See the License for the specific language governing permissions and 
-// limitations under the License. 
-
 package com.microsoft.bingads.examples;
 
 import java.rmi.*;
@@ -219,9 +205,6 @@ public class Targets {
              DeviceOSTargetBid deviceOSTargetBid = new DeviceOSTargetBid();
              deviceOSTargetBid.setBidAdjustment(20);
              deviceOSTargetBid.setDeviceName("Tablets");
-             ArrayOfstring osNames = new ArrayOfstring();
-             osNames.getStrings().add("Other");
-             deviceOSTargetBid.setOSNames(osNames);
              ArrayOfDeviceOSTargetBid deviceOSTargetBids = new ArrayOfDeviceOSTargetBid();
              deviceOSTargetBids.getDeviceOSTargetBids().add(deviceOSTargetBid);
              deviceOSTarget.setBids(deviceOSTargetBids);
@@ -640,14 +623,6 @@ public class Targets {
              {
                  System.out.printf("\tBidAdjustment: %d\n", bid.getBidAdjustment());
                  System.out.printf("\tDeviceName: %s\n", bid.getDeviceName());
-                 if (bid.getOSNames() != null)
-                 {
-                     System.out.println("\tOSNames:");
-                     for (java.lang.String osname : bid.getOSNames().getStrings())
-                     {
-                         System.out.printf("\t\t%s\n", osname);
-                     }
-                 }
                  System.out.println();
              }
          }
@@ -769,14 +744,6 @@ public class Targets {
              {
                  System.out.printf("\tBidAdjustment: %d\n", bid.getBidAdjustment());
                  System.out.printf("\tDeviceName: %s\n", bid.getDeviceName());
-                 if (bid.getOSNames() != null)
-                 {
-                     System.out.printf("\tOSNames:\n");
-                     for (java.lang.String osname : bid.getOSNames().getStrings())
-                     {
-                         System.out.printf("\t\t%s\n", osname);
-                     }
-                 }
                  System.out.println();
              }
          }
