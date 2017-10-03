0c496ae33620d4e9d0e56d0cb3500d6899f0c862
Eric Urban <eur@microsoft.com>
2017-10-02 19:44:01 -0700

    remove v9 and v10 samples

--- examples/BingAdsDesktopApp/src/main/java/com/microsoft/bingads/examples/v9/ExampleBase.java
+++
@@ -1,9 +0,0 @@
-package com.microsoft.bingads.examples.v9;
-
-import java.rmi.RemoteException;
-
-public class ExampleBase extends com.microsoft.bingads.examples.ExampleBase {
-	
-	public ExampleBase(){}
-	
-}
