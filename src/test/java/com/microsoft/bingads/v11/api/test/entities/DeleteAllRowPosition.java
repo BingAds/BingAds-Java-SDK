bd606dfe4afc756554b310c9b065f7cafc4f5e75
qitia <qitia@microsoft.com>
2018-11-13 09:49:39 +0000

    remove v11 UT

--- src/test/java/com/microsoft/bingads/v11/api/test/entities/DeleteAllRowPosition.java
+++
@@ -1,8 +0,0 @@
-package com.microsoft.bingads.v11.api.test.entities;
-
-public enum DeleteAllRowPosition {
-
-    IN_THE_BEGINNING,
-    IN_THE_MIDDLE,
-    IN_THE_END;
-}
--- src/test/java/com/microsoft/bingads/v11/api/test/entities/EqualityComparerWithDescription.java
+++
@@ -1,7 +0,0 @@
-package com.microsoft.bingads.v11.api.test.entities;
-
-public interface EqualityComparerWithDescription<T> {
-    Boolean equals(T x, T y);
-    
-    String getDescription(T obj);
-}
