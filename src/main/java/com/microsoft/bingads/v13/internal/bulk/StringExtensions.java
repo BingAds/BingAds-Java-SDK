9926d6fd6acb281ea8ab0477ff2198cf10d54521
Markus Heiden <markus@markusheiden.de>
2019-08-09 11:18:31 +0200

    Fix handling of text asset links

--- src/main/java/com/microsoft/bingads/v13/internal/bulk/StringExtensions.java
+++ src/main/java/com/microsoft/bingads/v13/internal/bulk/StringExtensions.java
@@ -1487,7 +1487,7 @@ public class StringExtensions {
 
         // The Asset Id
         @JsonProperty
-        public long id;
+        public Long id;
 
         // The Asset Text
         @JsonProperty
@@ -1603,7 +1603,7 @@ public class StringExtensions {
         List<AssetLink> assetLinks = arrayOfAssetLink
                 .getAssetLinks()
                 .stream()
-                .filter(s -> "TextAsset".equals(s.getAsset().getType()))
+                .filter(link -> link.getAsset() instanceof TextAsset)
                 .collect(Collectors.toList());
         if (assetLinks.size() == 0) {
             return null;
